c6ec2a14b8498218b7949511ee6903bb39c1b2bc
Chris <chrisieaks@gmail.com>
2018-03-02 10:42:32 -0500

    json

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 var http = require('http');
+
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, function(){
+    console.log(`Listening on http:/localhost:${PORT}`);
+});
